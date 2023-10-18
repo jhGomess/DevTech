a1c894a462d530a093bdf26934b650c2991b9418
jhGomess <jhuangomes@hotmail.com>
2023-10-18 02:00:46 -0300

    adicionado mais uma opção na page menu, opção funcionário.

--- script.js
+++ script.js
@@ -14,12 +14,24 @@ function f() {
 
 function q() {
   document.getElementsByClassName("drop")[0].classList.toggle("down")
-  document.getElementsByClassName("flash")[0].classList.toggle("gone")
+  document.getElementsByClassName("arrow2")[0].classList.toggle("gone")
   if (document.getElementsByClassName("drop")[0].classList.contains("down")) {
     setTimeout(function () {
       document.getElementsByClassName("drop")[0].style.overflow = "visible"
     }, 500)
   } else {
     document.getElementsByClassName("drop")[0].style.overflow = "hidden"
+  }
+}
+
+function w() {
+  document.getElementsByClassName("func")[0].classList.toggle("down")
+  document.getElementsByClassName("arrow3")[0].classList.toggle("gone")
+  if (document.getElementsByClassName("func")[0].classList.contains("down")) {
+    setTimeout(function () {
+      document.getElementsByClassName("func")[0].style.overflow = "visible"
+    }, 500)
+  } else {
+    document.getElementsByClassName("func")[0].style.overflow = "hidden"
   }
 }
