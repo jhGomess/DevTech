296e4e85bcf3507120d0efde9cb2ffb36171884e
jhGomess <jhuangomes@hotmail.com>
2023-10-18 02:08:50 -0300

    adicionado mais 3 opções na page menu, opção folha, rel e help.

--- script.js
+++ script.js
@@ -35,3 +35,39 @@ function w() {
     document.getElementsByClassName("func")[0].style.overflow = "hidden"
   }
 }
+
+function e() {
+  document.getElementsByClassName("folha")[0].classList.toggle("down")
+  document.getElementsByClassName("arrow4")[0].classList.toggle("gone")
+  if (document.getElementsByClassName("folha")[0].classList.contains("down")) {
+    setTimeout(function () {
+      document.getElementsByClassName("folha")[0].style.overflow = "visible"
+    }, 500)
+  } else {
+    document.getElementsByClassName("folha")[0].style.overflow = "hidden"
+  }
+}
+
+function j() {
+  document.getElementsByClassName("rel")[0].classList.toggle("down")
+  document.getElementsByClassName("arrow5")[0].classList.toggle("gone")
+  if (document.getElementsByClassName("rel")[0].classList.contains("down")) {
+    setTimeout(function () {
+      document.getElementsByClassName("rel")[0].style.overflow = "visible"
+    }, 500)
+  } else {
+    document.getElementsByClassName("rel")[0].style.overflow = "hidden"
+  }
+}
+
+function b() {
+  document.getElementsByClassName("help")[0].classList.toggle("down")
+  document.getElementsByClassName("arrow6")[0].classList.toggle("gone")
+  if (document.getElementsByClassName("help")[0].classList.contains("down")) {
+    setTimeout(function () {
+      document.getElementsByClassName("help")[0].style.overflow = "visible"
+    }, 500)
+  } else {
+    document.getElementsByClassName("help")[0].style.overflow = "hidden"
+  }
+}
