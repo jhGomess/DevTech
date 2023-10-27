c26b7bb32742032c4846da38ff8ce56e2f343bed
jhGomess <jhuangomes@hotmail.com>
2023-10-27 03:11:52 -0300

    formulário de consulta de departamento desenvolvido e estilizado com sucesso.

---
+++ javascript/consultarDepartamento.js
@@ -0,0 +1,6 @@
+function performSearch() {
+  var searchTerm = document.getElementById("searchDepartament").value
+  // Aqui você pode adicionar a lógica para processar a pesquisa, por exemplo, redirecionar para uma página de resultados.
+  var searchInput = document.getElementById("searchDepartament")
+  searchInput.focus()
+}
