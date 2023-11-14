0e586aaac085e44fc2db66b69f8fdc58664a05ed
jhGomess <jhuangomes@hotmail.com>
2023-11-13 23:05:07 -0300

    ids da page cadastro de empresa recebidos no js.

---
+++ cadastroEmpresa.js
@@ -0,0 +1,31 @@
+let company = document.getElementById("company")
+let labelCompany = document.getElementById("labelCompany")
+let validCompany = false
+
+let cnpj = document.getElementById("cnpj")
+let labelCnpj = document.getElementById("labelCnpj")
+let validCnpj = false
+
+let id = document.getElementById("id")
+let labelId = document.getElementById("labelId")
+let validId = false
+
+let phone = document.getElementById("phone")
+let labelPhone = document.getElementById("labelPhone")
+let validPhone = false
+
+let emailCompany = document.getElementById("emailCompany")
+let labelEmailCompany = document.getElementById("labelEmailCompany")
+let validEmailCompany = false
+
+let address = document.getElementById("address")
+let labelAddress = document.getElementById("labelAddress")
+let validAddress = false
+
+let city = document.getElementById("city")
+let labelCity = document.getElementById("labelCity")
+let validCity = false
+
+let state = document.getElementById("state")
+let labelState = document.getElementById("labelState")
+let validState = false
