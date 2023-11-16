92fb932e59600a6d6a5e163fb0b7658869706eda
jhGomess <jhuangomes@hotmail.com>
2023-11-15 22:33:06 -0300

    vinculado os ids dos inputs no javascript.

---
+++ cadastroDepartamento.js
@@ -0,0 +1,28 @@
+let departament = document.getElementById("departament")
+let labelDepartament = document.getElementById("labelDepartament")
+let validDepartament = false
+
+let Responsible = document.getElementById("Responsible")
+let labelResponsible = document.getElementById("labelResponsible")
+let validResponsible = false
+
+let idDepartament = document.getElementById("idDepartament")
+let labelIdDepartament = document.getElementById("labelIdDepartament")
+let validIdDepartament = false
+
+let phoneDepartament = document.getElementById("phoneDepartament")
+let labelPhoneDepartament = document.getElementById("labelPhoneDepartament")
+let validPhoneDepartament = false
+
+let emailDepartament = document.getElementById("emailDepartament")
+let labelEmailDepartament = document.getElementById("labelEmailDepartament")
+let validEmailDepartament = false
+
+let description = document.getElementById("description")
+let labelDescription = document.getElementById("labelDescription")
+let validDescription = false
+
+let msgError = document.getElementById("msgError")
+let msgSucess = document.getElementById("msgSucess")
+
+let registerDepartament = document.getElementById("createdDepartament")
