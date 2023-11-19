6b960a4e917e392cbb4159ffebb463c247988b86
jhGomess <jhuangomes@hotmail.com>
2023-11-19 14:58:25 -0300

    ids da parte de cadastros de funcionários vinculado no javaScript.

---
+++ cadastroFunc.js
@@ -0,0 +1,76 @@
+let func = document.getElementById("func")
+let labelFunc = document.getElementById("labelFunc")
+let validFunc = false
+
+let idFunc = document.getElementById("idFunc")
+let labelIdFunc = document.getElementById("labelIdFunc")
+let validIdFunc = false
+
+let phoneFunc = document.getElementById("phoneFunc")
+let labelPhoneFunc = document.getElementById("labelPhoneFunc")
+let validPhoneFunc = false
+
+let cpf = document.getElementById("cpf")
+let labelCpf = document.getElementById("labelCpf")
+let validCpf = false
+
+let rg = document.getElementById("rg")
+let labelRg = document.getElementById("labelRg")
+let validRg = false
+
+let emailFunc = document.getElementById("emailFunc")
+let labelEmailFunc = document.getElementById("labelEmailFunc")
+let validEmailFunc = false
+
+let addressFunc = document.getElementById("addressFunc")
+let labelAddressFunc = document.getElementById("labelAddressFunc")
+let validAddressFunc = false
+
+let cityFunc = document.getElementById("cityFunc")
+let labelCityFunc = document.getElementById("labelCityFunc")
+let validCityFunc = false
+
+let stateFunc = document.getElementById("stateFunc")
+let labelStateFunc = document.getElementById("labelStateFunc")
+let validStateFunc = false
+
+let departmentFunc = document.getElementById("departmentFunc")
+let labelDepartmentFunc = document.getElementById("labelDepartmentFunc")
+let validDepartmentFunc = false
+
+let office = document.getElementById("office")
+let labelOffice = document.getElementById("labelOffice")
+let validOffice = false
+
+let baseSalary = document.getElementById("baseSalary")
+let labelBaseSalary = document.getElementById("labelBaseSalary")
+let validBaseSalary = false
+
+let date_Ad = document.getElementById("date_Ad")
+let labelDate_Ad = document.getElementById("labelDate_Ad")
+let validDate_Ad = false
+
+let dayWorked = document.getElementById("dayWorked")
+let labelDayWorked = document.getElementById("labelDayWorked")
+let validDayWorked = false
+
+let workedHours = document.getElementById("workedHours")
+let labelWorkedHours = document.getElementById("labelWorkedHours")
+let validWorkedHours = false
+
+let transportation = document.getElementById("transportation")
+let labelTransportation = document.getElementById("labelTransportation")
+let validTransportation = false
+
+let food = document.getElementById("food")
+let labelFood = document.getElementById("labelFood")
+let validFood = false
+
+let health = document.getElementById("health")
+let labelHealth = document.getElementById("labelHealth")
+let validHealth = false
+
+let registerFunc = document.getElementById("registerFunc")
+let consultFunc = document.getElementById("consultFunc")
+let alterFunc = document.getElementById("alterFunc")
+let deleteFunc = document.getElementById("deleteFunc")
