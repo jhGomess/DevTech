dbec2e47c5e53d3b24402a4816ab70aa2adfc4b5
jhGomess <jhuangomes@hotmail.com>
2023-11-23 23:10:36 -0300

    inicializado funções na page emitir holerite.

---
+++ emitirHolerite.js
@@ -0,0 +1,58 @@
+let initial_dt = document.getElementById("initial_dt")
+let final_dt = document.getElementById("final_dt")
+
+let processHol = document.getElementById("processHol")
+
+let company = document.getElementById("company")
+let validCompany = false
+
+let cnpj = document.getElementById("cnpj")
+let validCnpj = false
+
+let address = document.getElementById("address")
+let validAddress = false
+
+let city = document.getElementById("city")
+let validCity = false
+
+let state = document.getElementById("state")
+let validState = false
+
+let departmentFunc = document.getElementById("departmentFunc")
+let validDepartmentFunc = false
+
+let func = document.getElementById("func")
+let validFunc = false
+
+let idFunc = document.getElementById("idFunc")
+let validIdFunc = false
+
+let baseSalary = document.getElementById("baseSalary")
+let validBaseSalary = false
+
+let dayWorked = document.getElementById("dayWorked")
+let validDayWorked = false
+
+let workedHours = document.getElementById("workedHours")
+let validWorkedHours = false
+
+let transportation = document.getElementById("transportation")
+let validTransportation = false
+
+let food = document.getElementById("food")
+let validFood = false
+
+let inss = document.getElementById("inss")
+let validInss = false
+
+let fgts = document.getElementById("fgts")
+let validFgts = false
+
+let irrf = document.getElementById("irrf")
+let validIrrf = false
+
+let netSalary = document.getElementById("netSalary")
+let validNetSalary = false
+
+let msgError = document.getElementById("msgError")
+let msgSucess = document.getElementById("msgSucess")
