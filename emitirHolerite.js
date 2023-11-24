c64aa477773a5a8211b38477d37e2d1bdebb3f32
jhGomess <jhuangomes@hotmail.com>
2023-11-24 03:49:43 -0300

    finalizado funções na page emitir holerite e baixar pdf.

--- emitirHolerite.js
+++ emitirHolerite.js
@@ -1,7 +1,12 @@
 let initial_dt = document.getElementById("initial_dt")
 let final_dt = document.getElementById("final_dt")
 
+let initial = document.getElementById("initial")
+let final = document.getElementById("final")
+let emission = document.getElementById("emission")
+
 let processHol = document.getElementById("processHol")
+const download = document.getElementById("download")
 
 let company = document.getElementById("company")
 let validCompany = false
@@ -56,3 +61,143 @@ let validNetSalary = false
 
 let msgError = document.getElementById("msgError")
 let msgSucess = document.getElementById("msgSucess")
+
+function processarHolerite() {
+  let searchFunc = document
+    .getElementById("searchFunc")
+    .value.trim()
+    .toLowerCase()
+
+  let listaFunc = JSON.parse(localStorage.getItem("listaFunc"))
+  let listaCalc = JSON.parse(localStorage.getItem("listaCalc"))
+  let listaCompany = JSON.parse(localStorage.getItem("listaCompany"))
+
+  if (listaFunc && listaFunc.length > 0 && listaCalc && listaCalc.length > 0) {
+    processHol.disabled = true
+    let funcConsultado = listaFunc.find(
+      (funcionario) =>
+        funcionario.idFuncCad === searchFunc ||
+        funcionario.funcCad.toLowerCase() === searchFunc
+    )
+
+    let initialConsult = initial_dt.value != 0
+    let finalConsult = final_dt.value != 0
+
+    if (funcConsultado && initialConsult && finalConsult) {
+      func.value = funcConsultado.funcCad
+      idFunc.value = funcConsultado.idFuncCad
+      departmentFunc.value = funcConsultado.departmentFuncCad
+      baseSalary.value = funcConsultado.baseSalaryCad
+      dayWorked.value = funcConsultado.dayWorkedCad
+      workedHours.value = funcConsultado.workedHoursCad * dayWorked.value
+      transportation.value = funcConsultado.transportationCad
+      food.value = funcConsultado.foodCad
+
+      let calcConsultado = listaCalc.find(
+        (calc) =>
+          calc.idFuncCad === funcConsultado.idFuncCad ||
+          calc.funcCad === funcConsultado.funcCad
+      )
+
+      if (calcConsultado) {
+        inss.value = calcConsultado.inssCad
+        fgts.value = calcConsultado.fgtsCad
+        irrf.value = calcConsultado.irrfCad
+        netSalary.value = calcConsultado.netSalaryCad
+      }
+
+      if (listaCompany && listaCompany.length > 0) {
+        company.value = listaCompany[0].companyCad
+        cnpj.value = listaCompany[0].cnpjCad
+        address.value = listaCompany[0].addressCad
+        city.value = listaCompany[0].cityCad
+        state.value = listaCompany[0].stateCad
+      }
+
+      initial.value = initial_dt.value
+      final.value = final_dt.value
+
+      let dataAtual = new Date()
+
+      let ano = dataAtual.getFullYear()
+      let mes = ("0" + (dataAtual.getMonth() + 1)).slice(-2)
+      let dia = ("0" + dataAtual.getDate()).slice(-2)
+
+      let dataFormatada = ano + "-" + mes + "-" + dia
+      emission.value = dataFormatada
+
+      msgSucess.setAttribute("style", "display: block")
+      msgSucess.innerHTML = "<strong>Processando Holerite...</strong>"
+
+      msgError.setAttribute("style", "display: none")
+      msgError.innerHTML = ""
+
+      setTimeout(() => {
+        processHol.disabled = false
+
+        msgSucess.setAttribute("style", "display: none")
+        msgSucess.innerHTML = ""
+
+        msgError.setAttribute("style", "display: none")
+        msgError.innerHTML = ""
+      }, 3000)
+    } else {
+      // cadastro incorreto
+      processHol.disabled = true
+
+      msgError.setAttribute("style", "display: block")
+      msgError.innerHTML = "<strong>Erro ao processar Holerite</strong>"
+
+      msgSucess.setAttribute("style", "display: none")
+      msgSucess.innerHTML = ""
+
+      setTimeout(() => {
+        processHol.disabled = false
+
+        msgSucess.setAttribute("style", "display: none")
+        msgSucess.innerHTML = ""
+
+        msgError.setAttribute("style", "display: none")
+        msgError.innerHTML = ""
+      }, 3000)
+    }
+  } else {
+    // cadastro incorreto
+    processHol.disabled = true
+
+    msgError.setAttribute("style", "display: block")
+    msgError.innerHTML = "<strong>Erro ao processar Holerite</strong>"
+
+    msgSucess.setAttribute("style", "display: none")
+    msgSucess.innerHTML = ""
+
+    setTimeout(() => {
+      processHol.disabled = false
+
+      msgSucess.setAttribute("style", "display: none")
+      msgSucess.innerHTML = ""
+
+      msgError.setAttribute("style", "display: none")
+      msgError.innerHTML = ""
+    }, 3000)
+  }
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
+}
+
+download.addEventListener("click", () => {
+  const holerite = document.getElementById("holerite").innerHTML
+
+  let janela = window.open("", "", "width=1200, heigth=1200")
+  janela.document.write("<html> <head>")
+  janela.document.write("<title>DevTech </title> ")
+  janela.document.write("<link/> </head>")
+  janela.document.write("<body>")
+  janela.document.write(holerite)
+  janela.document.write("</body> </html>")
+  janela.document.close()
+  janela.print()
+})
